9fd2ba4d286219199e10e6f1881b7e8d6af67b5a
Rozy-ai <jumayewrozy@gmail.com>
2022-10-24 03:35:38 +0500

    add api test

---
+++ app/Http/Controllers/Api/ApiMainServiceController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Controller;
+
+class ApiMainServiceController extends Controller
+{
+    public function buses() 
+    {
+        return response()->json([
+            'bus1' => [
+                'name' => 'Hundaý New Super Aero Citi',
+                'seats' => 27,
+                'price' => 130,
+                'byujet_price' => 80,
+            ],
+            'bus2' => [
+                'name' => 'Iran-Hodro 0-457',
+                'seats' => 34,
+                'price' => 110,
+                'byujet_price' => 70,
+            ],
+            'bus3' => [
+                'name' => 'PAZ-32054',
+                'seats' => 23,
+                'price' => 85,
+                'byujet_price' => 50,
+            ],
+        ]);
+    }
+
+    public function trucks() 
+    {
+        return response()->json([
+            'truck1' => [
+                'name' => 'KAMAZ-6520',
+                'capacity' => 20,
+                'price' => 130,
+            ],
+            'truck2' => [
+                'name' => 'KAMAZ-65115',
+                'capacity' => 15,
+                'price' => 105,
+            ],
+            'truck3' => [
+                'name' => 'KAMAZ-65117',
+                'capacity' => 14,
+                'price' => 115,
+            ],
+            'truck3' => [
+                'name' => 'Mersedes Bens',
+                'capacity' => 27,
+                'price' => 160,
+            ],
+        ]);
+    }
+
+    public function pay(Request $request)
+    {
+        $validator = Validator::validate($request->all(),
+        [
+            // 'payment_type_id'=>'required|exists:payment_types,id',
+            // 'amount'=>'required|regex:/^\d+(\.\d{1,2})?$/',
+            // 'amount_used'=>'regex:/^\d+(\.\d{1,2})?$/',
+            // // 'paying_way'=>'required|exists:paying_ways,id',
+            // 'return_url' => 'required|url',
+        ]);
+         return $request->all();die;
+       
+    }
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ApiMainServiceController;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/buses',[ApiMainServiceController::class,'buses'])->name('buses');
+Route::get('/trucks',[ApiMainServiceController::class,'trucks'])->name('trucks');
+Route::post('/pay',[ApiMainServiceController::class,'pay'])->name('pay');
